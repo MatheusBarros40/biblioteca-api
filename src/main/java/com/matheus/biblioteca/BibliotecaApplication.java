e69c57ef5bdba275e5ba8ad454fd466b82ccc29d
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-08-24 21:28:06 -0300

    Implementando perfil de teste

--- src/main/java/com/matheus/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/matheus/biblioteca/BibliotecaApplication.java
@@ -12,26 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class BibliotecaApplication implements CommandLineRunner {
-
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
+public class BibliotecaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "informatica", "livros de TI");
-
-		Livro l1 = new Livro(null, "CLean Code", "Robert Martin", "Lorem ipsum", cat1);
-
-		cat1.getLivros().addAll(Arrays.asList(l1));
-
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-	}
 }
---
+++ src/main/java/com/matheus/biblioteca/config/TestConfig.java
@@ -0,0 +1,20 @@
+package com.matheus.biblioteca.config;
+
+import com.matheus.biblioteca.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public void instanciaDB(){
+        this.dbService.instanciaDB();
+
+    }
+}
